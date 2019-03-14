f55747430c74be8e560433da3b6bab10d6b932be
Jamie Ly <jamie.ly@gmail.com>
2019-03-13 20:14:18 -0400

    Initial Commit

---
+++ Tracer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tracer
+//
+//  Created by Ly, Jamie on 3/13/19.
+//  Copyright © 2019 Ly, Jamie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
