d144b8ab2c1459e56f85ab0cd3449c8a174e18e4
Jamie Ly <jamie.ly@gmail.com>
2019-03-13 20:16:55 -0400

    Added blank view

---
+++ Tracer/TraceView.swift
@@ -0,0 +1,21 @@
+//
+//  TraceView.swift
+//  Tracer
+//
+//  Created by Ly, Jamie on 3/13/19.
+//  Copyright © 2019 Ly, Jamie. All rights reserved.
+//
+
+import UIKit
+
+class TraceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
