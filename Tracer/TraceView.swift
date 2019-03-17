2dfa024c0371a2d0e288d9e85a542f4b47236647
Jamie Ly <jamie.ly@gmail.com>
2019-03-16 21:20:36 -0400

    draw expected path

--- Tracer/TraceView.swift
+++ Tracer/TraceView.swift
@@ -15,15 +15,69 @@ struct Line {
 
 class TraceView: UIView {
     private var lines: Array<Line>
+    private var _expectedPath: Array<CGPoint>
+    private var expectedPathView: UIImageView
+    
+    var expectedPath: Array<CGPoint> {
+        get { return _expectedPath }
+        set {
+            _expectedPath = newValue
+            drawExpectedPath(points: newValue)
+        }
+    }
+    
+    private func drawExpectedPath(points: Array<CGPoint>) {
+        UIGraphicsBeginImageContext(expectedPathView.bounds.size)
+        
+        guard var last = points.first,
+            let context = UIGraphicsGetCurrentContext() else {
+            print("There should be at least one point")
+            return
+        }
+        
+        context.setFillColor(UIColor.red.cgColor)
+        context.fill(expectedPathView.bounds)
+        
+        context.setStrokeColor(UIColor.blue.cgColor)
+        points[1..<points.count].forEach { pt in
+            context.move(to: last)
+            context.addLine(to: pt)
+            context.strokePath()
+            last = pt
+        }
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        expectedPathView.image = image
+    }
     
     required init?(coder aDecoder: NSCoder) {
         lines = Array<Line>()
+        _expectedPath = Array<CGPoint>()
+        expectedPathView = UIImageView(coder: aDecoder)!
+        
         super.init(coder: aDecoder)
+        commonInit()
     }
     
     override init(frame: CGRect) {
         lines = Array<Line>()
+        _expectedPath = Array<CGPoint>()
+        expectedPathView = UIImageView(frame: frame)
+        
         super.init(frame: frame)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        addSubview(expectedPathView)
+        expectedPath = [
+            CGPoint(x: 1, y: 1),
+            CGPoint(x: 100, y: 100),
+            CGPoint(x: 1, y: 200),
+            CGPoint(x: 100, y: 300)
+        ]
+        expectedPathView.alpha = 0.5
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
