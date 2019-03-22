65bfa05eefb6e4fd924e6d44aae0e10350716b8c
Jamie Ly <jamie.ly@gmail.com>
2019-03-21 21:33:43 -0400

    New project

---
+++ Tracer/Tracer.h
@@ -0,0 +1,19 @@
+//
+//  Tracer.h
+//  Tracer
+//
+//  Created by Ly, Jamie on 3/21/19.
+//  Copyright © 2019 Ly, Jamie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tracer.
+FOUNDATION_EXPORT double TracerVersionNumber;
+
+//! Project version string for Tracer.
+FOUNDATION_EXPORT const unsigned char TracerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tracer/PublicHeader.h>
+
+
